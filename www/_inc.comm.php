d7fd23af6c8e079aa52972894b41a597f72da024
sidazhong <sida9567@gmail.com>
2020-09-08 14:52:04 -0700

    sida

--- www/_inc.comm.php
+++ www/_inc.comm.php
@@ -1,21 +1,18 @@
 <?php
 // 系统版本号,根据实际部署实例的不同，分别定义
-define( 'systemVersion', 'www.domain.com' );
-
-//定义是否为生产环境
-define( 'PRODUCTION', '0');
+define( 'systemVersion', 'local.haiwainew.com' );
 
 //设置用于命令行调用的服务器变量
-if(empty($_SERVER['HTTP_HOST']))$_SERVER['HTTP_HOST']="www.domain.com";
+if(empty($_SERVER['HTTP_HOST']))$_SERVER['HTTP_HOST']="local.haiwainew.com";
 
 // 字体路径
-define( 'SRCROOT', "/Your/Location/framework/source" );
+define( 'SRCROOT', "/Home/sida/source" );
 
 // 定义根路径
 if(!defined('DOCUROOT')) define( 'DOCUROOT', dirname( __FILE__ ) );
 
 // 定义CDN配置
-define( 'CDNSERVER', 'cdn.domain.com' );
+define( 'CDNSERVER', 'local.haiwainew.com' );
 
 // 定义数据库配置
 define( 'DBSETTING', DOCUROOT.'/inc.db.php' );
@@ -25,37 +22,33 @@ require_once( DOCUROOT."/vendor/autoload.php" );
 include( DOCUROOT."/include/_functions.php" );
 
 // 定义当前服务器默认时区
-define( 'TIMEZONE',  'America/Los_Angeles'); 
+define( 'TIMEZONE',  'UTC-0800');//TODO PHP daylight saving time detection
 
 //设置调试IP
 define( 'DEBUGIP', '127.0.0.1' );
 
-// 测试用户信息
-define( 'USERSESS', "_inc.sess.php" );
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// 全局变量存储参数
-//$_GlobalConfig = array( 'host' => '127.0.0.1', 'port' => '11211' );
 
-//加载多项目子目录
-//$_GlobalSystem = array( 'admin','space','service','office' );
 
-// 定义全局配置文件,不使用数据库定义多站模块
-//define( 'GLOBALCONF', DOCUROOT.'/inc.conf.php' );
 
-//使用集中式模板
-//define( 'centreModel', true);
 
-//启用语言支持
-//define( 'LANGBASE', isset($_COOKIE['lang'])?$_COOKIE['lang']:conf('global','lid') );
 
-//数据库查询限量
-//define( 'SqlMaxMemorySize', 16777216 );
 
-// 加载程序配置文件
-include( DOCUROOT."/include/_init.php" );
 
-//用于调试
-debug::g();
-define( 'AppDebug','1');
-//define( 'SmartyDebug','1');
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
+
--- www/_inc.db.php
+++ www/_inc.db.php
@@ -1,60 +1,51 @@
 <?php
 return array(
-	'prefix'=>'mini',//数据库前缀
-	'mysqlMasterSlave'=>false,//是否启用mysql主从分离功能
-
-	'database'=>array(
-		//默认的主数据库，
-		'main'=>array(
-			//mysql主从结构中的主库
-			'master'=>'localhost',
-
-			//mysql主从结构中的从库，可以指定多个，mysqlMasterSlave设置为tru时生效
-			'slave'=>array('localhost','localhost'),
-
-			//可以指定数据库，或参考下面示例自动加载前缀、自动调用键名等方式调用指定的数据库
-			'db'=>'mini_main',
-
-			//是否默认使用分表功能,0/1
-			'multiTb'=>0,
-		),
-		
-		'article'=>array(
-			'master'=>'localhost',
-			'slave'=>array('localhost','localhost','localhost'),
-			'db'=>'_article'//根据加载前缀自动生成的数据库
-		),
-		
-		'blog'=>array(
-			'master'=>'localhost',
-			'slave'=>array('localhost','localhost','localhost'),
-			'db'=>''//根据键名自动生成的数据库
-		),
-			
-		'news' => array (
-				'master' => 'localmongo',
-				'db' => '_news'
-		),
-	),
-	
-	'server'=>array(
-		//指定一组服务器
-		'localhost'=>array(
-			'dbdriver'=> 'mysql',
-			'server'=> '127.0.0.1',
-			'user'=> 'root',
-			'password'=> '***',
-			'charset'=> 'utf8',
-			'pconnect'=> 0,
-		),
-			
-		//Mongo 数据库
-		'localmongo' => array (
-				'dbdriver' => 'mongo',
-				'user'=> 'root',
-				'password'=> '***',
-				'server' => 'localhost',
-		),
-	),
+    'prefix'=>'wxc',//数据库前缀
+    'mysqlMasterSlave'=>false,//是否启用mysql主从分离功能
+    
+    'database'=>array(
+        //默认的主数据库，
+        'main'=>array(
+            //mysql主从结构中的主库
+            'master'=>'mainDB',
+            
+            //mysql主从结构中的从库，可以指定多个，mysqlMasterSlave设置为tru时生效
+            'slave'=>array('mainDB'),
+            
+            //可以指定数据库，或参考下面示例自动加载前缀、自动调用键名等方式调用指定的数据库
+            'db'=>'whw_main',
+            
+            //是否默认使用分表功能,0/1
+            'multiTb'=>1,
+        ),
+        
+        'article'=>array(
+            'master'=>'dbnews',
+            'slave'=>array('mainDB'),
+            'db'=>'whw_article'//根据键名自动生成的数据库
+        ),
+        
+        'blog'=>array(
+            'master'=>'dbnews',
+            'slave'=>array('mainDB'),
+            'db'=>'whw_blog'//根据键名自动生成的数据库
+        ),
+        
+        'account'=>array(
+            'master'=>'dbnews',
+            'slave'=>array('mainDB'),
+            'db'=>'whw_account'//根据键名自动生成的数据库
+        ),
+    ),
+    'server'=>array(
+        'mainDB'=>array(
+            'dbdriver'=> 'mysql',
+            'server'=> '104.154.95.120',
+            'user'=> 'wxc',
+            'password'=> 'f7!P#x$l9Hs8',
+            'charset'=> 'utf8',
+            'pconnect'=> 0,
+        ),
+    ),
 );
 ?>
