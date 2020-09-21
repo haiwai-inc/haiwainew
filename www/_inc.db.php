0bf888e084ac7f0508c57c84ab45fc0f84079bcf
someone <someone@someplace.com>
2020-09-20 21:46:02 -0700

    sida

--- www/_inc.db.php
+++ www/_inc.db.php
@@ -20,27 +20,58 @@ return array(
         ),
         
         'article'=>array(
-            'master'=>'dbnews',
+            'master'=>'mainDB',
             'slave'=>array('mainDB'),
             'db'=>'whw_article'//根据键名自动生成的数据库
         ),
         
         'blog'=>array(
-            'master'=>'dbnews',
+            'master'=>'mainDB',
             'slave'=>array('mainDB'),
             'db'=>'whw_blog'//根据键名自动生成的数据库
         ),
         
         'account'=>array(
-            'master'=>'dbnews',
+            'master'=>'mainDB',
             'slave'=>array('mainDB'),
             'db'=>'whw_account'//根据键名自动生成的数据库
         ),
+        
+        'legacy_blog'=>array(
+            'master'=>'DB4',
+            'slave'=>array('DB4'),
+            'db'=>'wxc_blog'//根据键名自动生成的数据库
+        ),
+        
+        'legacy_members'=>array(
+            'master'=>'DB3',
+            'slave'=>array('DB3'),
+            'db'=>'wxc_members'//根据键名自动生成的数据库
+        ),
+        
     ),
     'server'=>array(
         'mainDB'=>array(
             'dbdriver'=> 'mysql',
             'server'=> '104.154.95.120',
+            'user'=> 'wxc',
+            'password'=> 'f7!P#x$l9Hs8',
+            'charset'=> 'utf8',
+            'pconnect'=> 0,
+        ),
+        
+        'DB3'=>array(
+            'dbdriver'=> 'mysql',
+            'server'=> '104.197.66.148',
+            'user'=> 'wxc',
+            'password'=> 'f7!P#x$l9Hs8',
+            'charset'=> 'utf8',
+            'pconnect'=> 0,
+        ),
+        
+        'DB4'=>array(
+            'dbdriver'=> 'mysql',
+            'server'=> '104.197.30.114',
             'user'=> 'wxc',
             'password'=> 'f7!P#x$l9Hs8',
             'charset'=> 'utf8',
