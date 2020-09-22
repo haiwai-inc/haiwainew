6bf76928f5e7497d76842f50bd0cd0b6ba3f4716
someone <someone@someplace.com>
2020-09-22 16:49:17 -0700

    s

--- www/admin/index.php
+++ www/admin/index.php
@@ -1,11 +1,3 @@
 <?php
 include "../inc.comm.php";
-
-if($_SERVER['HTTP_HOST']=='cp.wenxuecity.com') go('/');
-
-define( 'AppName', 'bbs' );
-
-$app = new Factory();
-$app->sess = true;
-$app->run('manager','admin');
 ?>
