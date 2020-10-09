241bf47def26904e35611508630e3e8f6fb2d1a9
someone <someone@someplace.com>
2020-10-09 11:51:57 -0700

    sida

--- www/account/api/passport.php
+++ www/account/api/passport.php
@@ -1,4 +1,9 @@
 <?php
+
+/**
+ * @author sida
+ * ['data'=>[],'msg'=>[],'status'=>true]
+ */
 class passport extends Api {
 
     public function __construct() {
@@ -9,10 +14,20 @@ class passport extends Api {
     /**
      * 检查登录状态
      */
-    public function login_status() {
+    public function login_status($userID) {
         $obj_account_user=load("account_user");
-        debug::d($obj_account_user);
-        return true;
+        $rs_account_user=$obj_account_user->getOne(['id','name','description','background','avatar'],['id'=>$userID]);
+        
+        $obj_blog_blogger=load("blog_blogger");
+        $rs_blog_blogger=$obj_blog_blogger->getOne(['id','userID','description','background'],['userID'=>$userID]);
+        
+        $rs=[
+            'status'=>true,
+            'msg'=>"",
+            'data'=>['account_user'=>$rs_account_user,'blog_blogger'=>$rs_blog_blogger],
+        ];
+        
+        return $rs;
     }
     
     
