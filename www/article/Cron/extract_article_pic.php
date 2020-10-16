455e35fe3f1ad9625915adfef5c17ae8b51579fd
someone <someone@someplace.com>
2020-10-16 16:49:22 -0700

    s

--- www/article/Cron/extract_article_pic.php
+++ www/article/Cron/extract_article_pic.php
@@ -12,7 +12,7 @@ class extract_article_pic{
         
         
         $lastid=0;
-        while( $rs_article_indexing = $this->obj_article_indexing->getAll("*",['is_pic'=>0,'order'=>['postID'=>'ASC'],'limit'=>20,'postID,>'=>$lastid,'visible'=>1]) ){
+        while( $rs_article_indexing = $this->obj_article_indexing->getAll("*",['treelevel'=>0,'is_pic'=>0,'order'=>['postID'=>'ASC'],'limit'=>20,'postID,>'=>$lastid,'visible'=>1]) ){
             foreach($rs_article_indexing as $k=>$v){
                 $lastid=$v['postID'];
                 $post_tbn=substr('0'.$v['userID'],-1);
