64e8906f01ed152aae1b3af73508644907dbc869
Roy_Yao <yaoyu.sina@gmail.com>
2020-10-21 18:57:47 -0700

    yy

--- vue/src/service/user.js
+++ vue/src/service/user.js
@@ -4,8 +4,8 @@ import API from "./api"
  * Temp user class
  */
 class User extends API{
-    username = "username"
-
+    username = "testUser"
+    avatar = ""
     printUsername(){
         console.log(this.username)
     }
@@ -26,6 +26,21 @@ class User extends API{
         }
         catch(e){
             console.log(e)
+            return false;
+        }
+    }
+    /**
+     * get user infor
+     * @param userID
+     */
+    async getUserInfo(userID){
+        try{
+            let res = await this.get("account/passport/login_status/?userID="+userID);
+            this.avatar = res.data.data.avatar
+            return res
+        }
+        catch(e){
+            console.log(e);
             return false;
         }
     }
