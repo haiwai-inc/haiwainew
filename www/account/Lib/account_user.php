1db7f65dd8b76f2e5ee083e650f42375f1821a5f
someone <someone@someplace.com>
2020-11-03 15:09:48 -0800

    s

--- www/account/Lib/account_user.php
+++ www/account/Lib/account_user.php
@@ -3,6 +3,7 @@ class account_user extends Model{
 	protected $tableName="user";
 	protected $dbinfo=array("config"=>"account","type"=>"MySQL");
 	
+	//在包含用户ID的数组里，补全用户基本信息
 	function get_basic_userinfo($rs,$hashID='id'){
 	    if(!empty($rs)){
 	        foreach($rs as $v){
