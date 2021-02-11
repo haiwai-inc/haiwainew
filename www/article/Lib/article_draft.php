aa45b7b4bd82992fbadbebb9c1105c9fbd5af64f
someone <someone@someplace.com>
2021-02-10 17:34:21 -0800

    s

--- www/article/Lib/article_draft.php
+++ www/article/Lib/article_draft.php
@@ -7,8 +7,8 @@ class article_draft extends Model{
 	    if(!empty($rs_article_draft)){
 	        foreach($rs_article_draft as $k=>$v){
 	            $rs_article_draft[$k]['postInfo_postID']['id']=$v['id'];
-	            $rs_article_draft[$k]['postInfo_postID']['title']=$v['id'];
-	            $rs_article_draft[$k]['postInfo_postID']['msgbody']=$v['id'];
+	            $rs_article_draft[$k]['postInfo_postID']['title']=$v['title'];
+	            $rs_article_draft[$k]['postInfo_postID']['msgbody']=$v['msgbody'];
 	            $rs_article_draft[$k]['postInfo_postID']['tags']=empty($v['tagID'])?[]:explode(",",$v['tagID']);
 	        }
 	    }
