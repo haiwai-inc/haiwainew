930644450ae220d723776f7b3a4e2b1a1760489e
someone <someone@someplace.com>
2021-02-22 18:37:20 -0800

    s

--- www/blog/api/user.php
+++ www/blog/api/user.php
@@ -154,7 +154,7 @@ class user extends Api {
             if(empty($check_account_follower)) {$this->error="此用户未在您的关注列表";$this->status=false;return false;}
             $followingID_account_follow[]=$followerID;
         }else{
-            $check_account_follow=$obj_account_follow->getAll("*",['followerID'=>$_SESSION['id']]);
+            $check_account_follow=$obj_account_follow->getAll("*",['order'=>['id'=>'DESC'],'limit'=>50,'followingID'=>$_SESSION['id']]);
             if(empty($rs_account_follower))  {$this->error="你还未关注任何用户";$this->status=false;return false;}
             foreach($rs_account_follower as $v){
                 $followingID_account_follow[]=$v['followerID'];
