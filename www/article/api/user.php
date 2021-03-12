dd5084274d89e5196670f1173e65fd8b84d4ddf8
someone <someone@someplace.com>
2021-03-11 17:43:40 -0800

    s

--- www/article/api/user.php
+++ www/article/api/user.php
@@ -139,7 +139,7 @@ class user extends Api {
             $obj_article_draft->remove(['id'=>$article_data['draftID']]);
         }
         
-        return true;
+        return $this->article_view($article_data['postID']);
     }
     
     /**
