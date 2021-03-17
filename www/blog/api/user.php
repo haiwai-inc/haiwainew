c27f70aafeaffb0165ccab0cb0c4f1ba9283e915
someone <someone@someplace.com>
2021-03-17 15:12:48 -0700

    s

--- www/blog/api/user.php
+++ www/blog/api/user.php
@@ -14,7 +14,7 @@ class user extends Api {
      */
     public function blogger_profile(){
         $obj_blog_blogger=load("blog_blogger");
-        $rs_blog_blogger=$obj_blog_blogger->getOne(['id','name','description','background'],['id'=>$_SESSION['id']]);
+        $rs_blog_blogger=$obj_blog_blogger->getOne(['id','name','description','background'],['id'=>$_SESSION['bloggerID']]);
         $rs_blog_blogger['first_letter']=substr(strings::subString($rs_blog_blogger['name'],1), 0, -3);
         
         return $rs_blog_blogger;
