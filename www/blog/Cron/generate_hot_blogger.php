392f32e52cf2f8d483efb6e1f940a4a6d87fc389
someone <someone@someplace.com>
2021-03-31 11:55:05 -0700

    s

--- www/blog/Cron/generate_hot_blogger.php
+++ www/blog/Cron/generate_hot_blogger.php
@@ -9,14 +9,8 @@ class generate_hot_blogger{
     function start(){
         $this->obj_blog_blogger=load("blog_blogger");
         $rs_blog_blogger=$this->obj_blog_blogger->getAll(['id','userID','count_follower','count_buzz','count_article','count_comment','count_read','description'],['status'=>1,'limit'=>200,'order'=>['count_read'=>'DESC']]);
-        if(!empty($rs_blog_blogger)){
-            foreach($rs_blog_blogger as $k=>$v){
-                $rs_blog_blogger[$k]['bloggerID']=$v['id'];
-            }
-            
-            $obj_memcache = func_initMemcached('cache01');
-            $obj_memcache->set("blog_hot_blogger",$rs_blog_blogger, 3600*24);
-        }
+        $obj_memcache = func_initMemcached('cache01');
+        $obj_memcache->set("blog_hot_blogger",$rs_blog_blogger, 3600*24);
     }
 }
 
