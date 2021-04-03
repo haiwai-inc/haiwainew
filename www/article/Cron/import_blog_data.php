45431d6f30220d9f203274c11bfc9d4b40ee27f6
someone <someone@someplace.com>
2021-04-02 17:20:58 -0700

    s

--- www/article/Cron/import_blog_data.php
+++ www/article/Cron/import_blog_data.php
@@ -14,6 +14,7 @@ class import_blog_data{
         for($i=1;$i<=12;$i++){
             $month=substr('00'.$i,-2);
             while( $rs_blog_legacy_202005_post = $obj_blog_tool->obj_blog_legacy_202005_post->getAll("*",['order'=>['postid'=>'ASC'],'limit'=>20,'postid,>'=>$lastid,'visible'=>1],"blog_2020{$month}_post") ){
+                $postID_legacy_hot_post=[];
                 foreach($rs_blog_legacy_202005_post as $k=>$v){
                     $lastid=$v['postid'];
                     $rs_import_post=$obj_blog_tool->import_post($v);
