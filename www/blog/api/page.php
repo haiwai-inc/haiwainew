5ab827616e609b7639d861ac2d622c8a23e75d8d
someone <someone@someplace.com>
2021-04-08 18:18:49 -0700

    s

--- www/blog/api/page.php
+++ www/blog/api/page.php
@@ -9,13 +9,11 @@ class page extends Api {
     /**
      * “别人的”博主信息页
      * 博主 信息
-     * @param integer $bloggerID
+     * @param integer $userID
      */
-    public function blogger_info($bloggerID){
-        if(empty($bloggerID))   {$this->error="此博主不存在";$this->status=false;return false;}
-        
+    public function blogger_info($userID){
         $obj_blog_blogger=load("blog_blogger");
-        $rs_blog_blogger=$obj_blog_blogger->getAll(['id','userID'],['id'=>$bloggerID,'status'=>1]);
+        $rs_blog_blogger=$obj_blog_blogger->getAll(['id','userID','bloggerID'],['userID'=>$userID,'status'=>1]);
         if(empty($rs_blog_blogger)) {$this->error="此博主不存在";$this->status=false;return false;}
         
         //添加用户信息
