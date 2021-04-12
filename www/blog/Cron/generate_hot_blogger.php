3dc303c15859c1d48f39eae374e8a35db9223341
someone <someone@someplace.com>
2021-04-12 15:48:25 -0700

    s

--- www/blog/Cron/generate_hot_blogger.php
+++ www/blog/Cron/generate_hot_blogger.php
@@ -8,7 +8,7 @@ func_checkCliEnv();
 class generate_hot_blogger{
     function start(){
         $this->obj_blog_blogger=load("blog_blogger");
-        $rs_blog_blogger=$this->obj_blog_blogger->getAll(['id','userID','count_follower','count_buzz','count_article','count_comment','count_read','description'],['status'=>1,'limit'=>200,'order'=>['count_read'=>'DESC']]);
+        $rs_blog_blogger=$this->obj_blog_blogger->getAll(['id','userID','count_buzz','count_article','count_comment','count_read','description'],['status'=>1,'limit'=>200,'order'=>['count_read'=>'DESC']]);
         $obj_memcache = func_initMemcached('cache01');
         $obj_memcache->set("blog_hot_blogger",$rs_blog_blogger, 3600*24);
     }
