2f84d0ae162136b2eb878aeaa7291da0ec2fb3c3
someone <someone@someplace.com>
2021-04-13 18:55:26 -0700

    s

--- www/article/Cron/import_blog_data.php
+++ www/article/Cron/import_blog_data.php
@@ -5,8 +5,11 @@ define( 'DOCUROOT',str_replace("/article/Cron","",dirname( __FILE__ )));
 include DOCUROOT.'/inc.comm.php';
 func_checkCliEnv();
 
+//汪老师https://blog.wenxuecity.com/myoverview/69027/
+//润涛阎https://blog.wenxuecity.com/myoverview/1666/
+
 class import_blog_data{
-    function start(){
+    function start($year){
         $obj_blog_tool=load("blog_tool");
         $obj_blog_tool->load_all_db();
         $lastid=0;
@@ -15,10 +18,16 @@ class import_blog_data{
             $month=substr('00'.$i,-2);
             
             //if($i==4){
-                while( $rs_blog_legacy_202005_post = $obj_blog_tool->obj_blog_legacy_202005_post->getAll("*",['order'=>['postid'=>'ASC'],'limit'=>20,'postid,>'=>$lastid,'visible,!='=>0],"blog_2021{$month}_post") ){
+                while( $rs_blog_legacy_202005_post = $obj_blog_tool->obj_blog_legacy_202005_post->getAll("*",['order'=>['postid'=>'ASC'],'limit'=>20,'postid,>'=>$lastid,'visible,!='=>0],"blog_{$year}{$month}_post") ){
                     $postID_legacy_hot_post=[];
                     foreach($rs_blog_legacy_202005_post as $k=>$v){
                         $lastid=$v['postid'];
+                        
+                        //汪老师
+                        if($v['blogid']!=69027){
+                            continue;
+                        }
+                        
                         $rs_import_post=$obj_blog_tool->import_post($v);
                         $postID_legacy_hot_post[]=$rs_import_post['article_new']['postID'];
                         
@@ -30,13 +39,15 @@ class import_blog_data{
                     $obj_article_noindex->fetch_and_insert($postID_legacy_hot_post);
                 }
             //}
+            
         }
         
     }
 }
 
+$year = empty($argv[1])?"2021":$argv[1];
 $obj = new import_blog_data();
-$obj->start();
+$obj->start($year);
 
 
 
