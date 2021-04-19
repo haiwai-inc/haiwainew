42b44eb99b8e39527e6dd2eb93656b926e46931a
someone <someone@someplace.com>
2021-04-19 16:02:57 -0700

    s

--- www/blog/Lib/blog_blogger.php
+++ www/blog/Lib/blog_blogger.php
@@ -69,39 +69,55 @@ class blog_blogger extends Model{
 	    return $rs;
 	}
 	
-	//转文章为博客类型
-	function to_blog_article($postID,$module_data){
-	    $fields_indexing=[
-	        'bloggerID'=>$module_data['bloggerID'],
-	    ];
-	    
-	    //文集
-	    $obj_blog_category=load("blog_category");
-	    $check_blog_category=$obj_blog_category->getOne("*",['bloggerID'=>$module_data['bloggerID'],'id'=>$module_data['categoryID']]);
-	    
-	    if(!empty($check_blog_category)){
+	//添加文章为博客类型
+	function add_blog_article($article_data,$module_data){
+	    //是否为公开文章
+	    if(!empty($module_data['is_publish'])){
+	        //同步文集计数
+	        $obj_blog_category=load("blog_category");
+	        $check_blog_category=$obj_blog_category->getOne("*",['bloggerID'=>$module_data['bloggerID'],'id'=>$module_data['categoryID']]);
 	        $obj_blog_category->update(['count_article'=>$check_blog_category['count_article']+1],['id'=>$check_blog_category['id']]);
-	        $fields_indexing['categoryID']=$check_blog_category['id'];
 	    }
 	    
 	    //修改索引表
 	    $obj_article_indexing=load("article_indexing");
-	    $obj_article_indexing->update($fields_indexing,['postID'=>$postID]);
+	    $obj_article_indexing->update(['bloggerID'=>$module_data['bloggerID'],'categoryID'=>$module_data['categoryID']],['postID'=>$article_data['postID']]);
 	    
-	    //修改博主计数信息
+	    //修改博主信息
 	    $time=times::getTime();
 	    $check_blogger=$this->getOne('*',['id'=>$module_data['bloggerID']]);
 	    $fields_blogger=[
 	        'update_date'=>$time,
-	        'update_ip'=>http::getIP()
+	        'update_ip'=>http::getIP(),
+	        'count_article'=>$check_blogger['count_article']+1,
+	        'update_type'=>'add_article',
 	    ];
-	    if(!empty($module_data['add'])){
-	        $fields_blogger['count_article']=$check_blogger['count_article']+1;
-	        $fields_blogger['update_type']='add_article';
-	    }
-	    if(!empty($module_data['edit'])){
-	        $fields_blogger['update_type']='edit_article';
-	    }
+	    $this->update($fields_blogger,['id'=>$module_data['bloggerID']]);
+	    
+	    //更新关注时间
+	    $obj_account_follow=load("account_follow");
+	    $obj_account_follow->update(['following_update'=>$time],["followingID"=>$check_blogger['userID']]);
+	}
+	
+	//添加博客文章
+	function update_blog_article($postID,$module_data){
+	    //同步文集计数
+	    $obj_blog_category=load("blog_category");
+	    $check_blog_category=$obj_blog_category->getOne("*",['bloggerID'=>$module_data['bloggerID'],'id'=>$module_data['categoryID']]);
+	    $obj_blog_category->update(['count_article'=>$check_blog_category['count_article']+1],['id'=>$check_blog_category['id']]);
+	    
+	    //修改索引表
+	    $obj_article_indexing=load("article_indexing");
+	    $obj_article_indexing->update(['bloggerID'=>$module_data['bloggerID'],'categoryID'=>$module_data['categoryID']],['postID'=>$postID]);
+	    
+	    //修改博主信息
+	    $time=times::getTime();
+	    $check_blogger=$this->getOne('*',['id'=>$module_data['bloggerID']]);
+	    $fields_blogger=[
+	        'update_date'=>$time,
+	        'update_ip'=>http::getIP(),
+	        'update_type'=>'edit_article'
+	    ];
 	    $this->update($fields_blogger,['id'=>$module_data['bloggerID']]);
 	    
 	    //更新关注时间
@@ -110,6 +126,22 @@ class blog_blogger extends Model{
 	}
 	
 	
+	//删除博客文章操作
+	function delete_blog_article($rs_article_indexing){
+	    //是否为隐藏目录
+	    if(!empty($rs_article_indexing['is_publish'])){
+	        $obj_blog_category=load("blog_category");
+	        $rs_blog_category=$obj_blog_category->getOne("*",['id'=>$rs_article_indexing['categoryID']]);
+	        
+	        //同步文集计数
+	        $obj_blog_category->update(["count_article"=>$rs_blog_category['count_article']-1],['id'=>$rs_article_indexing['categoryID']]);
+	        
+	        //同步博客计数
+	        $rs_blog_blogger=$this->getOne("*",['id'=>$rs_article_indexing['bloggerID']]);
+	        $this->update(['count_article'=>$rs_blog_blogger['count_article']-1],['id'=>$rs_article_indexing['bloggerID']]);
+	    }
+	    
+	}
 	
 	
 	
