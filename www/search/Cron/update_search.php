da3b7c2c2dab1145c4691e4fa86e3bdfc58a0f8d
yundanangel <yundanangel@outlook.com>
2021-05-04 11:04:19 -0700

    fix

--- www/search/Cron/update_search.php
+++ www/search/Cron/update_search.php
@@ -7,9 +7,11 @@ func_checkCliEnv();
 
 class update_search{
     public $action;
+    public $target;
     
-    function __construct($action){
+    function __construct($action, $target){
         $this->action=$action;
+        $this->target=$target;
         $this->obj_article_indexing=load("article_indexing");
         $this->obj_search_article=load("search_article_index");
         $this->boj_search_article_noindex=load("search_article_noindex");
@@ -19,12 +21,17 @@ class update_search{
         if($this->action=="all"){
             //更新全部
             $lastid=0;
-        }else{
+        }else if($this->action=="single"){
+            $this->addOneToNoIndex($this->target);
+            return;
+        }
+        else{
             //更新10分钟以前
             $edit_date=times::getTime()-600;
             $rs_lastid=$this->obj_article_indexing->getOne(['postID'],['edit_date,>'=>$edit_date,'order'=>['postID'=>"ASC"]]);
             $lastid=empty($rs_lastid)?2147483647:$rs_lastid['postID']-1;
         }
+        
         
         $count=0;
         while($rs_article_indexing=$this->obj_article_indexing->getAll("*",['order'=>['postID'=>'ASC'],'limit'=>200,'postID,>'=>$lastid]) ){
@@ -45,9 +52,24 @@ class update_search{
         
         echo "totally {$count}\n";
     }
+
+
+    function addOneToNoIndex($target){
+        $rs_article_indexing=$this->obj_article_indexing->getAll('*', ['postID'=>$target]);
+        foreach($rs_article_indexing as $k=>$v){
+            $lastid=$v['postID'];
+            
+            //补全帖子所有分表信息
+            $rs_article_indexing=$this->obj_article_indexing->get_basic_articleinfo($rs_article_indexing,$k,$v);
+            
+            echo $lastid."\n";
+        }
+        $this->boj_search_article_noindex->add_new_articles($rs_article_indexing);
+    }
 }
 $argv[1]=empty($argv[1])?"":$argv[1];
-$obj=new update_search($argv[1]);
+$argv[2]=empty($argv[2])?"":$argv[2];
+$obj=new update_search($argv[1], $argv[2]);
 $obj->start();
 
 
