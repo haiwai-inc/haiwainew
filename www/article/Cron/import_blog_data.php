651865e1ed6a6a82226d41e12f2e317a7b2df3dd
someone <someone@someplace.com>
2021-05-23 20:58:21 -0700

    s

--- www/article/Cron/import_blog_data.php
+++ www/article/Cron/import_blog_data.php
@@ -27,7 +27,7 @@ class import_blog_data{
                     echo $lastid."_".$month."\n";
                     
                     //汪老师 userID=942857 blogid=69027 basecode=211   7324
-                    if($v['userid']==1066812){
+                    if($v['userid']==866909){
                         //主贴
                         $v['date']=substr($v['dateline'],0,7); //=========================主贴时间
                         $rs_import_post=$obj_blog_tool->import_post($v);
