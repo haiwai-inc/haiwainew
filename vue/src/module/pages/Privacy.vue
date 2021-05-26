6db0833556689ebd34fadfa412257ca85eacae22
xiaocao811 <di.cheng66@gmail.com>
2021-05-26 15:19:45 -0700

    d

--- vue/src/module/pages/Privacy.vue
+++ vue/src/module/pages/Privacy.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-  <!-- 内容放在这里 -->
-  </div>
+	<main role="main" class="container">
+	   <div class="row">
+	      <div class="col-md-12 ">
+	         <h3 class="pb-3 mt-4 mb-4 border-bottom">Terms of Use and Privacy Policy</h3>
+	         <div>
+	            <p>You share content on the site at your own risk. Although Haiwai blog Platforms may allow you to set privacy options that limit access to your information, please be aware that no security measures are perfect or impenetrable. Further, Haiwai blog cannot control the actions of other Users with whom you share your profile and information. Therefore, the Haiwai blog does not guarantee that User Content you post on the site will not be used improperly or viewed by other individuals.</p>
+	            <h3>Security</h3>
+	            <p>Participation in this Haiwai blog platform is voluntary. You accept responsibility for maintaining the confidentiality of your login information, and for all activities that occur under your login. You should immediately notify the platform of any unauthorized use of your login or account or any other breach of security. You agree that the Haiwai is not liable in any way for any loss or damage arising from your failure to comply with this Section.</p>
+	            <ol></ol>
+	         </div>
+	      </div>
+	   </div>
+	</main>
 </template>
 <script>
 
--- vue/src/module/pages/Tou.vue
+++ vue/src/module/pages/Tou.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
-  <!-- 内容放在这里 -->
-  </div>
+	<main role="main" class="container">
+	   <div class="row">
+	      <div class="col-md-12 ">
+	         <h3 class="pb-3 mt-4 mb-4 border-bottom">Member Conduct</h3>
+	         <div>
+	            <blockquote></blockquote>
+	            <p>You understand that all information, data, messages or other materials (“Content”), whether publicly posted or privately transmitted, are the sole responsibility of the person posting or transmitting such Content. This means that you, and not the Haiwai blog platform, accept full responsibility for all Content that you post, email, transmit or otherwise make available via the site.</p>
+	            <h3>You agree not to use the site in a manner to:</h3>
+	            <ul>
+	               <li>post or otherwise make available threats of violence or harm to any individual or group; obscene, vulgar, or lewd material; fraudulent statements; terms commonly understood to constitute profanity or abusive or degrading slurs or epithets;</li>
+	               <li>impersonate any actual person, entity, corporation, government or representative thereof;</li>
+	               <li>post or otherwise make available any Content that you do not have a right to make available, including but not limited to material restricted by intellectual property or other proprietary rights, material that is classified or otherwise legally protected, or material protected by contractual or fiduciary obligations;</li>
+	               <li>post or otherwise make available information about yourself or other individuals that could be misused, such as for identity theft (this restriction is intended to protect you and others) </li>
+	               <li>post or otherwise make available any Content that you do not have a right to make available, including but not limited to material restricted by intellectual property or other proprietary rights, material that is classified or otherwise legally protected, or material protected by contractual or fiduciary obligations;</li>
+	               <li>post or otherwise make available any malware or other material that contains software viruses or any other computer code, files or programs designed to interrupt, destroy or limit the functionality of any computer software or hardware or telecommunications equipment; or designed to gather sensitive information or gain privileged access to private computer systems; or stalk, harass or otherwise harm another individual or group. </li>
+	            </ul>
+	            <p>You agree not to use any portion of the site for commercial gain, including its Content, the use of the site, or access to the site, including by commercially reproducing, duplicating, copying, selling, or reselling.</p>
+	            <ol></ol>
+	         </div>
+	         <div>
+	            <h3>Haiwai Content Management</h3>
+	            <p>The Haiwai blog platform does not endorse any user-posted Content or otherwise suggest or guarantee the accuracy, integrity or quality of such Content. You understand that by visiting this site, you may be exposed to Content that is offensive, indecent or objectionable. You agree that under no circumstances is the Haiwai liable in any way for any non-Haiwai-generated Content (including, but not limited to, any errors or omissions in any Content), or for any loss or damage of any kind incurred as a result of the use of any Content posted, emailed, transmitted or otherwise made available via the site.</p>
+	         </div>
+	      </div>
+	   </div>
+	</main>
 </template>
 <script>
 
