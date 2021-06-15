8be77d4f8f1ba75b7560d32f82a2bfe721fff18d
someone <someone@someplace.com>
2021-06-15 14:22:45 -0700

    s

--- www/account/Lib/account_user_email.php
+++ www/account/Lib/account_user_email.php
@@ -33,7 +33,7 @@ class account_user_email extends Model{
             return false;
         }
         
-        $data['domain']=FILE_DOMIAN;
+        $data['domain']=FILE_DOMAIN;
         $this->smartyObj->assign("data",$data);
         $content=$this->smartyObj->fetch(DOCUROOT."/account/Tpl/register_verified.html");
         $title="海外博客，注册用户";
@@ -48,7 +48,7 @@ class account_user_email extends Model{
             return false;
         }
         
-        $data['domain']=FILE_DOMIAN;
+        $data['domain']=FILE_DOMAIN;
         $this->smartyObj->assign("data",$data);
         $content=$this->smartyObj->fetch(DOCUROOT."/account/Tpl/forgotpwd.html");
         $title="海外博客，忘记密码";
