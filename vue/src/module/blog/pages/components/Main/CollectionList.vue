63cc9dd25c906a9e89ff43254ef51ec4506cff70
Roy_Yao <yaoyu.sina@gmail.com>
2021-06-15 10:34:49 -0700

    yy

--- vue/src/module/blog/pages/components/Main/CollectionList.vue
+++ vue/src/module/blog/pages/components/Main/CollectionList.vue
@@ -77,7 +77,7 @@ export default {
       var bubbles=['blog_home_manage','blog_home_profile','blog_home_setting'];
       for(let i=0;i<bubbles.length;i++){
         let type = bubbles[i]
-        if(this.user.userinfo.bubble.user[type]==1){
+        if(this.user.userinfo.bubble.user[type]==1 && this.user.userinfo.bloggerID==this.data[0].bloggerID){
           this.bubbles[type]=true;
           return
         }else{
