55682301bbfbf216b0ca0040e85264ece197fb40
someone <someone@someplace.com>
2021-06-23 15:22:12 -0700

    s

--- www/blog/api/page.php
+++ www/blog/api/page.php
@@ -561,7 +561,7 @@ class page extends Api {
             'bloggerID'=>$rs_blog_category['bloggerID'],
             'categoryID'=>$id,
             'treelevel'=>0,
-            'limit'=>30,
+            'limit'=>15,
             "order"=>['create_date'=>"DESC"],
         ];
         if(!empty($lastID)){
