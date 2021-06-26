59639f9bfd264bacd77bc3ddbd05e591681c4513
xiaocao811 <di.cheng66@gmail.com>
2021-06-26 14:06:43 -0700

    d

--- vue/src/module/blog/pages/components/Main/BlogHelp.vue
+++ vue/src/module/blog/pages/components/Main/BlogHelp.vue
@@ -1,14 +1,20 @@
 <template>
             <div class="box mt-4 pl-3 bolger-box sticky-top help">
               <span v-html="icons.helpcenter"></span>
-              <span class="ml-3 mb-3 h6"><a href="http://www1.haiwai.com/blog/c/618/4393" class="small">帮助中心</a></span>
+              <span class="ml-3 mb-3 "><a href="http://www1.haiwai.com/blog/c/618/4393" class="title font-weight-bold text-dark">帮助中心</a></span>
               <div class=row>
-               <div class="col-6 mt-3 text-secondary"><a href="http://www1.haiwai.com/blog/p/1448485" class="small">如何发布博客文章</a></div>
-                <div class="col-6 mt-3 text-secondary"><a href="http://www1.haiwai.com/blog/p/1448486" class="small">如何上传图片</a></div>
+              <div class="col-12 mt-3 text-secondary"><a href="https://www1.haiwai.com/blog/p/1636880" class="small">复制文学城博客和相关问题解答</a></div>
+              </div>
+              <div class=row>
+               <div class="col-12 mt-3 text-secondary"><a href="https://www1.haiwai.com/blog/p/1636901" class="small">如何在海外注册和登入</a></div>
+              </div>
+              <div class=row>
+               <div class="col-6 mt-3 text-secondary"><a href="https://www1.haiwai.com/blog/p/1448485" class="small">如何发博文</a></div>
+                <div class="col-6 mt-3 text-secondary"><a href="https://www1.haiwai.com/blog/p/1448486" class="small">如何上传图片</a></div>
               </div>
               <div class=row>  
-                <div class="col-6 mt-3  text-secondary"><a href="http://www1.haiwai.com/blog/p/1448487" class="small">如何发视频</a></div>
-                <div class="col-6 mt-3 text-secondary"><a href="http://www1.haiwai.com/blog/p/1448488" class="small">如何贴音乐</a></div>
+                <div class="col-6 mt-3  text-secondary"><a href="https://www1.haiwai.com/blog/p/1448487" class="small">如何发视频</a></div>
+                <div class="col-6 mt-3 text-secondary"><a href="https://www1.haiwai.com/blog/p/1448488" class="small">如何贴音乐</a></div>
               </div>
             </div>
 </template>
@@ -27,9 +33,13 @@ export default {
 };
 </script>
 <style>
-.bolger-box a 
-{color: #6c757d!important;
+.help a 
+{color: #6c757d;
 font-size:1rem}
+
+.help a.title
+{font-size:1.3rem} 
+
 .bolger-box {
         background-color: #f6f9fc;
         border-radius: 16px;
